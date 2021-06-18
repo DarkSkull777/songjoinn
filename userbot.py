82a12a4d6f712fd76ac67e7f77edbe26914c382d
DarkSkull777 <59839225+DarkSkull777@users.noreply.github.com>
2021-06-19 01:33:21 +0700

    Create userbot.py

---
+++ userbot.py
@@ -0,0 +1,10 @@
+# import logging
+from pyrogram import Client, idle
+
+app = Client("tgvc")
+# logging.basicConfig(level=logging.INFO)
+app.start()
+print('>>> SKULL7-USERBOT TELAH AKTIF🔥🔥')
+idle()
+app.stop()
+print('\n>>> USERBOT STOPPED')
